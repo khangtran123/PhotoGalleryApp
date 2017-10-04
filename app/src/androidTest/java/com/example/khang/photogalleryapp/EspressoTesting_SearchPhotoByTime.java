938eea86a2390febef545fb0c2eb6bbda8c06169
Khang <khangh.tran@hotmail.com>
2017-10-04 00:22:58 -0700

    I commented out the Espresso Test Class for now since I have just edited the AdvancedSearch class adding the start and end date dialog.

---
+++ app/src/androidTest/java/com/example/khang/photogalleryapp/EspressoTesting_SearchPhotoByTime.java
@@ -0,0 +1,43 @@
+/*package com.example.khang.photogalleryapp;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.pressKey;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by Khang on 28/09/2017.
+
+
+@RunWith(AndroidJUnit4.class)
+public class EspressoTesting_SearchPhotoByTime {
+
+    @Rule
+    public ActivityTestRule<MainMenu> mActivityRule =
+            new ActivityTestRule<>(MainMenu.class);
+
+    @Test
+    public void ensureButtonisClicked() {
+        onView(withId(R.id.btnSearch)).perform(click());
+    }
+
+}
+
+*/
--- app/src/main/java/com/example/khang/photogalleryapp/AdvancedSearch.java
+++ app/src/main/java/com/example/khang/photogalleryapp/AdvancedSearch.java
@@ -1,6 +1,10 @@
 package com.example.khang.photogalleryapp;
 
+import android.app.DatePickerDialog;
+import android.widget.DatePicker;
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,40 +15,69 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.Calendar;
+
 public class AdvancedSearch extends AppCompatActivity {
     int currentYear, currentMonth, currentDay;
     static final int DATE_DIALOG_ID = 0;
     Button btnStartDate, btnEndDate, btnSearch, btnMenu;
     CheckBox chkLocation, chkTimeframe, chkKeywrd;
-    TextView startDate, endDate;
-    EditText enterLocation, enterKeyword;
+    EditText enterLocation, enterKeyword, startDate, endDate;
+    DatePickerDialog datePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advanced_search);
-        btnStartDate = (Button) findViewById(R.id.btnStartDate);
-        btnEndDate = (Button) findViewById(R.id.btnEndDate);
+
         btnSearch = (Button) findViewById(R.id.btnSearch);
         btnMenu = (Button) findViewById(R.id.btnMenu);
-        startDate = (TextView) findViewById(R.id.txtStart);
-        endDate = (TextView) findViewById(R.id.txtEnd);
-        chkLocation = (CheckBox) findViewById(R.id.chkLocation);
+
+        //This is for the Date
         chkTimeframe = (CheckBox) findViewById(R.id.chkDate);
+        startDate = (EditText) findViewById(R.id.txtStartDate);
+        endDate = (EditText) findViewById(R.id.txtEndDate);
+
+        chkLocation = (CheckBox) findViewById(R.id.chkLocation);
         chkKeywrd = (CheckBox) findViewById(R.id.chkKeyword);
         enterLocation = (EditText) findViewById(R.id.txtLocation);
         enterKeyword = (EditText) findViewById(R.id.txtKeyword);
-        /*btnStartDate.setOnClickListener(new View.OnClickListener() {
-            @Override
+
+        startDate.setOnClickListener(new View.OnClickListener(){
+            //this gets the current date, month and year from calendar
+            @RequiresApi(api = Build.VERSION_CODES.N)
             public void onClick(View v) {
-                showDialog(DATE_DIALOG_ID);
+                final Calendar c = Calendar.getInstance();
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+                //This is the dialog box that pops up to allow user to pick a date
+                datePickerDialog = new DatePickerDialog(AdvancedSearch.this, new DatePickerDialog.OnDateSetListener(){
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth){
+                        startDate.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
             }
         });
-        if(chkLocation.isChecked()){
-            enterLocation.setVisibility(View.VISIBLE);
-        } else{
-            enterLocation.setVisibility(View.GONE);
-        }*/
+
+        endDate.setOnClickListener(new View.OnClickListener(){
+            //this gets the current date, month and year from calendar
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+                //This is the dialog box that pops up to allow user to pick a date
+                datePickerDialog = new DatePickerDialog(AdvancedSearch.this, new DatePickerDialog.OnDateSetListener(){
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth){
+                        endDate.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
 
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
